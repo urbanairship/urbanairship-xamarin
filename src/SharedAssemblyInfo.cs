d5c57bb9aa4fbbd42bf6f3588ceefb5bf1fea873
Ryan Lepinski <ryan.lepinski@urbanairship.com>
2016-10-05 17:41:39 -0700

    Use shared AssemblyInfo. Clean up extra versions strings

---
+++ src/SharedAssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+// Information about this assembly is defined by the following attributes.
+// Change them to the values specific to your project.
+
+[assembly: AssemblyTitle ("AirshipBindings")]
+[assembly: AssemblyDescription ("")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("Urban Airship")]
+[assembly: AssemblyProduct ("")]
+[assembly: AssemblyCopyright ("Copyright 2009-2016 Urban Airship Inc. All rights reserved.")]
+[assembly: AssemblyTrademark ("")]
+[assembly: AssemblyCulture ("")]
+
+// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
+// The form "{Major}.{Minor}.*" will automatically update the build and revision,
+// and "{Major}.{Minor}.{Build}.*" will update just the revision.
+
+[assembly: AssemblyVersion ("3.0.0")]
+
