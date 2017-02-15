0f180c1abd0608d1f19f87c46c2deed5d063ceeb
Max Del Giudice <maxdelgiudice@gmail.com>
2017-02-15 12:58:30 -0800

    Add PCL error message

--- src/AirshipBindings.Portable/AirshipBindings.Portable/Airship.cs
+++ src/AirshipBindings.Portable/AirshipBindings.Portable/Airship.cs
@@ -5,6 +5,7 @@
 using System;
 using System.Collections.Generic;
 
+
 namespace UrbanAirship.Portable
 {
 	public class Airship : IAirship
@@ -27,49 +28,77 @@ namespace UrbanAirship.Portable
 		/// Indicates whether notifications are enabled.
 		/// </summary>
 		/// <value><c>true</c> if notifications are enabled; otherwise, <c>false</c>.</value>
-		public bool UserNotificationsEnabled { get; set; }
+		public bool UserNotificationsEnabled 
+		{ 
+			get { throw new NotImplementedException(PCL.BaitWithoutSwitchMessage); } 
+			set { throw new NotImplementedException(PCL.BaitWithoutSwitchMessage); }
+		}
 
 		/// <summary>
 		/// Gets the tags currently set for the device.
 		/// </summary>
 		/// <value>The tags.</value>
-		public IEnumerable<string> Tags { get { return null; } }
+		public IEnumerable<string> Tags 
+		{ 
+			get { throw new NotImplementedException(PCL.BaitWithoutSwitchMessage); }
+		}
 
 		/// <summary>
 		/// Get the channel ID for the device.
 		/// </summary>
 		/// <value>The channel identifier.</value>
-		public string ChannelId { get { return null; } }
+		public string ChannelId 
+		{ 
+			get { throw new NotImplementedException(PCL.BaitWithoutSwitchMessage); }
+		}
 
 		/// <summary>
 		/// Indicates whether location is enabled
 		/// </summary>
 		/// <value><c>true</c> if location is enabled; otherwise, <c>false</c>.</value>
-		public bool LocationEnabled { get; set; }
+		public bool LocationEnabled 
+		{ 
+			get { throw new NotImplementedException(PCL.BaitWithoutSwitchMessage); }
+			set { throw new NotImplementedException(PCL.BaitWithoutSwitchMessage); }
+		}
 
 		/// <summary>
 		/// Indicates whether background location updates are allowed.
 		/// </summary>
 		/// <value><c>true</c> if background location updates are allowed; otherwise, <c>false</c>.</value>
-		public bool BackgroundLocationAllowed { get; set; }
+		public bool BackgroundLocationAllowed 
+		{ 
+			get { throw new NotImplementedException(PCL.BaitWithoutSwitchMessage); }
+			set { throw new NotImplementedException(PCL.BaitWithoutSwitchMessage); }
+		}
 
 		/// <summary>
 		/// Gets or sets the named user ID.
 		/// </summary>
 		/// <value>The named user ID.</value>
-		public string NamedUser { get; set; }
+		public string NamedUser 
+		{ 
+			get { throw new NotImplementedException(PCL.BaitWithoutSwitchMessage); }
+			set { throw new NotImplementedException(PCL.BaitWithoutSwitchMessage); }
+		}
 
 		/// <summary>
 		/// Edit the device tags.
 		/// </summary>
 		/// <value>A Push.TagEditor for editing device tags.</value>
-		public Push.TagEditor EditDeviceTags() { return null; }
+		public Push.TagEditor EditDeviceTags() 
+		{ 
+			throw new NotImplementedException(PCL.BaitWithoutSwitchMessage); 
+		}
 
 		/// <summary>
 		/// Add a custom event
 		/// </summary>
 		/// <param name="customEvent">The custom event to add.</param>
-		public void AddCustomEvent(Analytics.CustomEvent customEvent) { }
+		public void AddCustomEvent(Analytics.CustomEvent customEvent) 
+		{
+			throw new NotImplementedException(PCL.BaitWithoutSwitchMessage);
+		}
 
 		/// <summary>
 		/// Associate a custom identifier. Previous identifiers will be replaced by the new identifiers
@@ -77,35 +106,55 @@ namespace UrbanAirship.Portable
 		/// </summary>
 		/// <param name="key">The custom key for the identifier.</param>
 		/// <param name="identifier">The value of the identifier, or `null` to remove the identifier.</param>
-		public void AssociateIdentifier(string key, string identifier) { }
+		public void AssociateIdentifier(string key, string identifier) 
+		{
+			throw new NotImplementedException(PCL.BaitWithoutSwitchMessage);
+		}
 
 		/// <summary>
 		/// Displays the message center.
 		/// </summary>
-		public void DisplayMessageCenter() { }
+		public void DisplayMessageCenter() 
+		{
+			throw new NotImplementedException(PCL.BaitWithoutSwitchMessage);
+		}
 
 		/// <summary>
 		/// Get the message center unread count.
 		/// </summary>
 		/// <value>The message center unread count.</value>
-		public int MessageCenterUnreadCount { get; private set; }
+		public int MessageCenterUnreadCount 
+		{ 
+			get { throw new NotImplementedException(PCL.BaitWithoutSwitchMessage); }
+			private set { throw new NotImplementedException(PCL.BaitWithoutSwitchMessage); }
+		}
 
 		/// <summary>
 		/// Get the total count of message center messages.
 		/// </summary>
 		/// <value>The message center count.</value>
-		public int MessageCenterCount { get; private set; }
+		public int MessageCenterCount 
+		{ 
+			get { throw new NotImplementedException(PCL.BaitWithoutSwitchMessage); }
+			private set { throw new NotImplementedException(PCL.BaitWithoutSwitchMessage); }
+		}
 
 		/// <summary>
 		/// Returns an editor for named user tag groups.
 		/// </summary>
 		/// <returns>A TagGroupEditor for named user tag groups.</returns>
-		public Push.TagGroupsEditor EditNamedUserTagGroups() { return null; }
+		public Push.TagGroupsEditor EditNamedUserTagGroups() 
+		{ 
+			throw new NotImplementedException(PCL.BaitWithoutSwitchMessage); 
+		}
 
 		/// <summary>
 		/// Returns an editor for channel tag groups.
 		/// </summary>
 		/// <returns>A TagGroupEditor for named user tag groups.</returns>
-		public Push.TagGroupsEditor EditChannelTagGroups() { return null; }
+		public Push.TagGroupsEditor EditChannelTagGroups() 
+		{ 
+			throw new NotImplementedException(PCL.BaitWithoutSwitchMessage);
+		}
 	}
 }
---
+++ src/AirshipBindings.Portable/AirshipBindings.Portable/PCL.cs
@@ -0,0 +1,8 @@
+﻿namespace UrbanAirship.Portable
+{
+	internal static class PCL
+	{
+		internal const string BaitWithoutSwitchMessage =
+			"The empty PCL implementation for Urbanairship was loaded. Check that you have added the urbanairship-portable package to each of your platform-specific projects.";
+	}
+}
