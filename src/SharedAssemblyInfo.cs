011cc4fd06e5a2bea6e1adcc4c20a86fe3e776da
Max Del Giudice <maxdelgiudice@gmail.com>
2017-03-22 15:12:33 -0700

    Release 4.2.1 (#56)
    * Update android SDK to 8.3.1
    
    * Release version 4.2.1

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -12,5 +12,5 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion ("4.2.0")]
+[assembly: AssemblyVersion ("4.2.1")]
 
