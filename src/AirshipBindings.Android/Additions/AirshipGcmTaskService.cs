c2dcaf75bd0fe94ad3322153312575b55baeeb0d
Max Del Giudice <maxdelgiudice@MacBook-Pro-4.local>
2017-05-04 11:07:22 -0700

    Fix missing manifest items

---
+++ src/AirshipBindings.Android/Additions/AirshipGcmTaskService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Android.App;
+
+namespace UrbanAirship.Job
+{
+	[Android.Runtime.Preserve(AllMembers = true)]
+	[Service(Exported=true, Permission="com.google.android.gms.permission.BIND_NETWORK_TASK_SERVICE")]
+	[IntentFilter(new[] { "com.google.android.gms.gcm.ACTION_TASK_READY" })]
+	public partial class AirshipGcmTaskService
+	{
+	}
+}
---
+++ src/AirshipBindings.Android/Additions/ChannelCaptureActivity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Android.App;
+
+namespace UrbanAirship
+{
+	[Android.Runtime.Preserve(AllMembers = true)]
+	[Activity]
+	public partial class ChannelCaptureActivity
+	{
+	}
+}
