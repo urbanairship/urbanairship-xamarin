e239a702692104a63390a098254bafeadc9b5af2
Ryan Lepinski <ryan.lepinski@urbanairship.com>
2017-06-21 15:33:39 -0700

    Update UA Android SDK

---
+++ src/AirshipBindings.Android/Additions/AirshpGcmTaskService.cs
@@ -0,0 +1,18 @@
+﻿/*
+ Copyright 2017 Urban Airship and Contributors
+*/
+
+using System;
+using Android.App;
+using Android.Content;
+
+namespace UrbanAirship.Job
+{
+	[Android.Runtime.Preserve(AllMembers = true)]
+	[Service]
+	[IntentFilter(new string[] { "com.google.android.gms.gcm.ACTION_TASK_READY" })]
+	public partial class AirshipGcmTaskService
+	{
+	}
+}
+
