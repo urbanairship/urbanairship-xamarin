f895a2dee2c7897f35d75b0458d71414cdc2b1db
Marc Sciglimpaglia <marc@urbanairship.com>
2018-07-20 15:00:24 -0700

    [MOBILE-243] version updates

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -12,5 +12,5 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion ("9.0.0")]
+[assembly: AssemblyVersion ("9.1.0")]
 
