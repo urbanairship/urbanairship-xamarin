41f7a781ccccac19c8215e4c36e6f02e2f1ad303
Brian Batchelder <brian@briansbrain.biz>
2019-06-24 11:29:52 -0700

    [MOBILE-640] Update versions. - I also broke SharedAssemblyInfo into 3 files, one for Android, one for iOS and one for cross-platform (PCL & .NET), as they have different versions.

--- src/SharedAssemblyInfo.iOS.cs
+++ src/SharedAssemblyInfo.iOS.cs
@@ -12,5 +12,5 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion ("9.4.1")]
+[assembly: AssemblyVersion ("11.0.0")]
 
