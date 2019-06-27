75e5048192fdb678a9600e9446b0654f5d392c6b
Brian Batchelder <BrianBatchelder@users.noreply.github.com>
2019-06-27 11:39:59 -0700

    [MOBILE-640] Xamarin: Update versions and changelog (#114)

---
+++ src/SharedAssemblyInfo.iOS.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+// Information about this assembly is defined by the following attributes.
+// Change them to the values specific to your project.
+
+
+[assembly: AssemblyCompany ("Urban Airship")]
+[assembly: AssemblyCopyright ("Copyright Urban Airship Inc. All rights reserved.")]
+
+// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
+// The form "{Major}.{Minor}.*" will automatically update the build and revision,
+// and "{Major}.{Minor}.{Build}.*" will update just the revision.
+
+[assembly: AssemblyVersion ("11.0.0")]
+
