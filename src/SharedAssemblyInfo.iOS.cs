8f7ed6e899258583f2a03c66eba99d0e7010b628
Brian Batchelder <brian@briansbrain.biz>
2020-01-15 15:49:17 -0800

    Update iOS version to 13.0.4

--- src/SharedAssemblyInfo.iOS.cs
+++ src/SharedAssemblyInfo.iOS.cs
@@ -12,5 +12,5 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion ("12.1.2")]
+[assembly: AssemblyVersion ("13.0.4")]
 
