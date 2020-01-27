ec0411b69c1a70a0101898925781c918c03c2637
Brian Batchelder <brian@briansbrain.biz>
2020-01-26 21:09:27 -0800

    Update AirshipBindings.iOS.Location and address API changes in sample app.

--- src/AirshipBindings.iOS.Location/ApiDefinitions.cs
+++ src/AirshipBindings.iOS.Location/ApiDefinitions.cs
@@ -6,6 +6,86 @@ using Foundation;
 using ObjCRuntime;
 
 namespace UrbanAirship {
+
+    [Static]
+    partial interface Constants
+    {
+
+
+        // extern NSString * const UAAnalyticsValueNone
+        [Field("UAAnalyticsValueNone", "__Internal")]
+        NSString UAAnalyticsValueNone { get; }
+
+        // extern UALocationEventUpdateType * const UALocationEventAnalyticsType
+        [Field("UALocationEventAnalyticsType", "__Internal")]
+        NSString UALocationEventAnalyticsType { get; }
+
+        // extern UALocationEventAnalyticsKey * const UALocationEventDesiredAccuracyKey
+        [Field("UALocationEventDesiredAccuracyKey", "__Internal")]
+        NSString UALocationEventDesiredAccuracyKey { get; }
+
+        // extern UALocationEventAnalyticsKey * const UALocationEventDistanceFilterKey
+        [Field("UALocationEventDistanceFilterKey", "__Internal")]
+        NSString UALocationEventDistanceFilterKey { get; }
+
+        // extern UALocationEventAnalyticsKey * const UALocationEventForegroundKey
+        [Field("UALocationEventForegroundKey", "__Internal")]
+        NSString UALocationEventForegroundKey { get; }
+
+        // extern UALocationEventAnalyticsKey * const UALocationEventHorizontalAccuracyKey
+        [Field("UALocationEventHorizontalAccuracyKey", "__Internal")]
+        NSString UALocationEventHorizontalAccuracyKey { get; }
+
+        // extern UALocationEventAnalyticsKey * const UALocationEventLatitudeKey
+        [Field("UALocationEventLatitudeKey", "__Internal")]
+        NSString UALocationEventLatitudeKey { get; }
+
+        // extern UALocationEventAnalyticsKey * const UALocationEventLongitudeKey
+        [Field("UALocationEventLongitudeKey", "__Internal")]
+        NSString UALocationEventLongitudeKey { get; }
+
+        // extern UALocationEventAnalyticsKey * const UALocationEventProviderKey
+        [Field("UALocationEventProviderKey", "__Internal")]
+        NSString UALocationEventProviderKey { get; }
+
+        // extern UALocationEventUpdateType * const UALocationEventUpdateTypeChange
+        [Field("UALocationEventUpdateTypeChange", "__Internal")]
+        NSString UALocationEventUpdateTypeChange { get; }
+
+        // extern UALocationEventUpdateType * const UALocationEventUpdateTypeContinuous
+        [Field("UALocationEventUpdateTypeContinuous", "__Internal")]
+        NSString UALocationEventUpdateTypeContinuous { get; }
+
+        // extern UALocationEventAnalyticsKey * const UALocationEventUpdateTypeKey
+        [Field("UALocationEventUpdateTypeKey", "__Internal")]
+        NSString UALocationEventUpdateTypeKey { get; }
+
+        // extern UALocationEventUpdateType * const UALocationEventUpdateTypeNone
+        [Field("UALocationEventUpdateTypeNone", "__Internal")]
+        NSString UALocationEventUpdateTypeNone { get; }
+
+        // extern UALocationEventUpdateType * const UALocationEventUpdateTypeSingle
+        [Field("UALocationEventUpdateTypeSingle", "__Internal")]
+        NSString UALocationEventUpdateTypeSingle { get; }
+
+        // extern UALocationEventAnalyticsKey * const UALocationEventVerticalAccuracyKey
+        [Field("UALocationEventVerticalAccuracyKey", "__Internal")]
+        NSString UALocationEventVerticalAccuracyKey { get; }
+
+        // extern UALocationServiceProviderType *const UALocationServiceProviderGps
+        [Field("UALocationServiceProviderGps", "__Internal")]
+        NSString UALocationServiceProviderGps { get; }
+
+        // extern UALocationServiceProviderType *const UALocationServiceProviderNetwork
+        [Field("UALocationServiceProviderNetwork", "__Internal")]
+        NSString UALocationServiceProviderNetwork { get; }
+
+        // extern UALocationServiceProviderType *const UALocationServiceProviderUnknown
+        [Field("UALocationServiceProviderUnknown", "__Internal")]
+        NSString UALocationServiceProviderUnknown { get; }
+
+    }
+
     // @protocol UALocationDelegate <NSObject>
     [Protocol, Model]
     [BaseType(typeof(NSObject))]
@@ -27,10 +107,16 @@ namespace UrbanAirship {
 
     interface IUALocationDelegate { }
 
-    // @interface UALocation : UAComponent <UALocationProviderDelegate>
+    // @interface UALocation : UAComponent <UALocationProvider>
     [BaseType(typeof(UAComponent))]
-    interface UALocation : IUALocationProviderDelegate
+    interface UALocation
     {
+        // + (null_unspecified instancetype)shared;
+        [Static]
+        [Export("shared")]
+        [New]
+        UALocation Shared ();
+
         // @property (getter=isAutoRequestAuthorizationEnabled, assign, readwrite, nonatomic) BOOL autoRequestAuthorizationEnabled;
         [Export("autoRequestAuthorizationEnabled")]
         bool AutoRequestAuthorizationEnabled { [Bind("isAutoRequestAuthorizationEnabled")] get; set; }
@@ -55,11 +141,6 @@ namespace UrbanAirship {
         [NullAllowed, Export("lastLocation")]
         CLLocation LastLocation { get; }
 
-        // + (null_unspecified UALocation *)sharedLocation;
-        [Static]
-        [Export("sharedLocation")]
-        UALocation SharedLocation ();
-
         // - (BOOL)isLocationOptedIn;
         [Export("isLocationOptedIn")]
         bool IsLocationOptedIn ();
@@ -67,10 +148,59 @@ namespace UrbanAirship {
         // - (BOOL)isLocationDeniedOrRestricted;
         [Export("isLocationDeniedOrRestricted")]
         bool IsLocationDeniedOrRestricted ();
-
-        // - (UALocationProviderPermissionStatus)locationPermissionStatus;
-        [Export("locationPermissionStatus")]
-        UALocationProviderPermissionStatus LocationPermissionStatus ();
     }
+
+    // @interface UALocationInfo : NSObject
+    [BaseType(typeof(NSObject))]
+    interface UALocationInfo
+    {
+        // @property (readonly, nonatomic) double latitude;
+        [Export("latitude")]
+        double Latitude { get; }
+
+        // @property (readonly, nonatomic) double longitude;
+        [Export("longitude")]
+        double Longitude { get; }
+
+        // @property (readonly, nonatomic) double horizontalAccuracy;
+        [Export("horizontalAccuracy")]
+        double HorizontalAccuracy { get; }
+
+        // @property (readonly, nonatomic) double verticalAccuracy;
+        [Export("verticalAccuracy")]
+        double VerticalAccuracy { get; }
+
+        // + (nonnull instancetype)infoWithLatitude:(double)latitude longitude:(double)longitude horizontalAccuracy:(double)horizontalAccuracy verticalAccuracy:(double)verticalAccuracy;
+        [Static]
+        [Export("infoWithLatitude:longitude:horizontalAccuracy:verticalAccuracy:")]
+        UALocationInfo Info (double latitude, double longitude, double horizontalAccuracy, double verticalAccuracy);
+    }
+
+    // @interface UALocationEvent : UAEvent
+    [BaseType(typeof(UAEvent))]
+    interface UALocationEvent
+    {
+
+        // + (nonnull UALocationEvent *) locationEventWithInfo:(nonnull UALocationInfo *)info providerType:(nullable UALocationServiceProviderType *)providerType desiredAccuracy:(nullable NSNumber *)desiredAccuracy distanceFilter:(nullable NSNumber *)distanceFilter;
+        [Static]
+        [Export("locationEventWithInfo:providerType:desiredAccuracy:distanceFilter:")]
+        UALocationEvent LocationEvent (UALocationInfo info, [NullAllowed] string providerType, [NullAllowed] NSNumber desiredAccuracy, [NullAllowed] NSNumber distanceFilter);
+
+        // + (nonnull UALocationEvent *) singleLocationEventWithInfo:(nonnull UALocationInfo *)info providerType: (nullable UALocationServiceProviderType *)providerType desiredAccuracy:(nullable NSNumber *)desiredAccuracy distanceFilter:(nullable NSNumber *)distanceFilter;
+        [Static]
+        [Export("singleLocationEventWithInfo:providerType:desiredAccuracy:distanceFilter:")]
+        UALocationEvent SingleLocationEvent (UALocationInfo info, [NullAllowed] string providerType, [NullAllowed] NSNumber desiredAccuracy, [NullAllowed] NSNumber distanceFilter);
+
+        // + (nonnull UALocationEvent *) significantChangeLocationEventWithInfo:(nonnull UALocationInfo *)info providerType: (nullable UALocationServiceProviderType *) providerType;
+        [Static]
+        [Export("significantChangeLocationEventWithInfo:providerType:")]
+        UALocationEvent SignificantChangeLocationEvent (UALocationInfo info, [NullAllowed] string providerType);
+
+        // + (nonnull UALocationEvent *) standardLocationEventWithInfo:(nonnull UALocationInfo *)info providerType: (nullable UALocationServiceProviderType *)providerType desiredAccuracy:(nullable NSNumber *)desiredAccuracy distanceFilter:(nullable NSNumber *)distanceFilter;
+        [Static]
+        [Export("standardLocationEventWithInfo:providerType:desiredAccuracy:distanceFilter:")]
+        UALocationEvent StandardLocationEvent (UALocationInfo info, [NullAllowed] string providerType, [NullAllowed] NSNumber desiredAccuracy, [NullAllowed] NSNumber distanceFilter);
+    }
+
 
 }
