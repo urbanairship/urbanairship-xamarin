c37956df62e95e2de414edc27ce61e9d6ca2ce82
Brian Batchelder <brian@briansbrain.biz>
2020-01-26 21:07:03 -0800

    Remove AirshipBindings.iOS and replace with AirshipBindings.iOS.Core.

--- src/AirshipBindings.iOS.Core/Properties/AssemblyInfo.cs
+++ src/AirshipBindings.iOS.Core/Properties/AssemblyInfo.cs
@@ -11,7 +11,6 @@ using Foundation;
 // When the attribute is present, the linker—if enabled—will process the assembly
 // even if you’re using the “Link SDK assemblies only” option, which is the default for device builds.
 
-[assembly: AssemblyTitle("AirshipBindings")]
-
 [assembly: LinkerSafe]
 
+[assembly: AssemblyTitle("AirshipBindings.iOS.Core")]
