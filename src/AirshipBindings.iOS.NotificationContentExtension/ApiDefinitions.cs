a3c19a8ea8b9f11481a6d56e550da05f1a098c8e
Brian Batchelder <brian@briansbrain.biz>
2020-01-26 21:40:50 -0800

    Add bindings for NotificationContentExtension module and create a new C# SampleContentExtension project.

---
+++ src/AirshipBindings.iOS.NotificationContentExtension/ApiDefinitions.cs
@@ -0,0 +1,29 @@
+/*
+ Copyright Airship and Contributors
+*/
+using CoreGraphics;
+using Foundation;
+using ObjCRuntime;
+using System;
+using UIKit;
+using UserNotifications;
+using WebKit;
+
+namespace UrbanAirship {
+
+    [Static]
+    partial interface Constants
+    {
+
+
+    }
+
+    // @interface UAContentExtensionViewController : UIViewController
+    [BaseType(typeof(UIViewController))]
+    interface UAContentExtensionViewController
+    {
+
+    }
+
+
+}
