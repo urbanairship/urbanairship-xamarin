58cc0a4c0501a156a315f8f62e8ccca9d5705564
Marc Sciglimpaglia <marc@urbanairship.com>
2020-03-16 16:31:16 -0700

    [MOBILE-1222] .NETStandard/portable attribute support (#130)
    * [MOBILE-1222] .NETStandard/portable attribute support
    
    * [MOBILE-1222] fix android dependencies

--- src/AirshipBindings.NETStandard/AirshipBindings.NETStandard.Abstractions/IAirship.cs
+++ src/AirshipBindings.NETStandard/AirshipBindings.NETStandard.Abstractions/IAirship.cs
@@ -51,5 +51,7 @@ namespace UrbanAirship.NETStandard
         Channel.TagGroupsEditor EditNamedUserTagGroups();
 
         Channel.TagGroupsEditor EditChannelTagGroups();
+        
+        Attributes.AttributeEditor EditAttributes();
     }
 }
