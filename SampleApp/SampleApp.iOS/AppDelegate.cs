c358136bffb9ec5e68de5957f7eeb34428ca62a6
Mouna Khemiri <mouna.khemiri@airship.com>
2020-05-07 10:04:26 +0200

    CR: Remove comment

--- SampleApp/SampleApp.iOS/AppDelegate.cs
+++ SampleApp/SampleApp.iOS/AppDelegate.cs
@@ -26,13 +26,6 @@ namespace SampleApp.iOS
     {
         private PushHandler pushHandler;
 
-        //
-        // This method is invoked when the application has loaded and is ready to run. In this 
-        // method you should instantiate the window, load the UI into it and then make the window
-        // visible.
-        //
-        // You have 17 seconds to return from this method, or iOS will terminate your application.
-        //
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
             this.FailIfSimulator();
