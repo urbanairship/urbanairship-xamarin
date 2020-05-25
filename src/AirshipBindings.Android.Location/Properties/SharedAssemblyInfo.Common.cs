c9fd5c2997987f0ca8be29ac99c0842c23078a04
Ognjen RISTANOVIC <ognjen.ristanovic@airship.com>
2020-05-25 15:01:04 +0200

    comments

--- src/AirshipBindings.Android.Location/Properties/SharedAssemblyInfo.Common.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-
-using UrbanAirship.Attributes;
-
-// Information about this assembly is defined by the following attributes.
-// Change them to the values specific to your project.
-
-// Cross-platform version of the plugin
-[assembly: UACrossPlatformVersion ("13.1.0")]
