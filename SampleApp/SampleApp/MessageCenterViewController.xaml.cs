025b533e35141c3ba041c9e744dd77c43b229377
Mouna Khemiri <mouna.khemiri@airship.com>
2020-06-25 14:51:57 +0200

    Add message center tab

---
+++ SampleApp/SampleApp/MessageCenterViewController.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace SampleApp
+{
+    public partial class MessageCenterViewController : ContentPage
+    {
+        public MessageCenterViewController()
+        {
+            InitializeComponent();
+        }
+    }
+}
