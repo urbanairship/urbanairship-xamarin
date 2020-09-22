b6a9690006d92847a537d288bbb0f812ceb7c0a9
Marc Sciglimpaglia <marc@urbanairship.com>
2020-09-22 16:55:46 -0700

    [MOBILE-1887] iOS module init

---
+++ src/AirshipBindings.iOS.MessageCenter/AirshipMessageCenter.cs
@@ -0,0 +1,17 @@
+﻿/*
+ Copyright Airship and Contributors
+*/
+
+namespace UrbanAirship
+{
+    /// <summary>
+    /// Entry point for initializing AirshipMessageCenter
+    /// </summary>
+    public class AirshipMessageCenter
+    {
+        /// <summary>
+        /// Initializes AirshipMessageCenter.
+        /// </summary>
+        public static void Init() { }
+    }
+}
