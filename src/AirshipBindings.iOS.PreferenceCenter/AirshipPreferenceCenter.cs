00cc8d8ae2a56751b57d0d789c8d6be2eade757b
Marc Sciglimpaglia <marc@urbanairship.com>
2021-11-22 17:28:42 -0800

    [MOBILE-2540] SDK 16.1.0 updates, preference center bindings

---
+++ src/AirshipBindings.iOS.PreferenceCenter/AirshipPreferenceCenter.cs
@@ -0,0 +1,17 @@
+﻿/*
+ Copyright Airship and Contributors
+*/
+
+namespace UrbanAirship
+{
+    /// <summary>
+    /// Entry point for initializing AirshipPreferenceCenter
+    /// </summary>
+    public class AirshipPreferenceCenter
+    {
+        /// <summary>
+        /// Initializes AirshipPreferenceCenter.
+        /// </summary>
+        public static void Init() { }
+    }
+}
