1d8b731e54e09c2b05474a9d618b606cb9e8d5df
Mouna Khemiri <mouna.khemiri@airship.com>
2021-12-02 15:40:18 +0100

    Update Xamarin to iOS 15.1.0

--- src/SharedAssemblyInfo.iOS.cs
+++ src/SharedAssemblyInfo.iOS.cs
@@ -17,5 +17,5 @@ using System.Reflection;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion ("16.1.0")]
+[assembly: AssemblyVersion ("16.1.1")]
 
