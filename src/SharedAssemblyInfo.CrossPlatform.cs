f30fb1c12499f036cd9a8dac69ab9db6f525d555
Mouna Khemiri <mouna.khemiri@airship.com>
2022-02-14 14:38:50 +0100

    Update net standard version to 16.0.0

--- src/SharedAssemblyInfo.CrossPlatform.cs
+++ src/SharedAssemblyInfo.CrossPlatform.cs
@@ -12,5 +12,5 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion ("15.0.4")]
+[assembly: AssemblyVersion ("16.0.0")]
 
