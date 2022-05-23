66af53c9bbb1a20a70a3f7c268061dcda1b87187
Mouna Khemiri <mouna.khemiri@airship.com>
2022-05-23 18:32:50 +0200

    Update to latest android SDK

--- src/SharedAssemblyInfo.Android.cs
+++ src/SharedAssemblyInfo.Android.cs
@@ -12,5 +12,5 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion ("16.4.0")]
+[assembly: AssemblyVersion ("16.5.0")]
 
