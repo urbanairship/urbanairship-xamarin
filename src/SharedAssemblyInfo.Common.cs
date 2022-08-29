dd3ea0f57b7284b693ed95778f9b8d82882e6c54
Ryan Lepinski <ryan.lepinski@urbanairship.com>
2022-08-29 15:44:02 -0700

    Release 16.2.0

--- src/SharedAssemblyInfo.Common.cs
+++ src/SharedAssemblyInfo.Common.cs
@@ -6,4 +6,4 @@ using UrbanAirship.Attributes;
 // Change them to the values specific to your project.
 
 // Cross-platform version of the plugin
-[assembly: UACrossPlatformVersion ("16.1.1")]
+[assembly: UACrossPlatformVersion ("16.2.0")]
