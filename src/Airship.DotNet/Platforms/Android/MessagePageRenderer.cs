d078bce8339236294aa5a18ef7b495f8f0858010
Josh Yaganeh <319444+jyaganeh@users.noreply.github.com>
2022-12-07 17:00:53 -0800

    Try to get Airship.DotNet shared lib working

---
+++ src/Airship.DotNet/Platforms/Android/MessagePageRenderer.cs
@@ -0,0 +1,195 @@
+﻿using System.ComponentModel;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Webkit;
+using Android.Widget;
+using UrbanAirship.MessageCenter;
+using UrbanAirship.MessageCenter.WebKit;
+
+using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Embedding;
+using Microsoft.Maui.Controls.Compatibility.Platform.Android;
+using Microsoft.Maui.Controls.Platform;
+using UrbanAirship.DotNet.MessageCenter;
+using WebView = Android.Webkit.WebView;
+
+namespace UrbanAirship.NETStandard.Android
+{
+    // TODO(maui): Convert to handler/mapper
+#pragma warning disable CS0618 // Type or member is obsolete
+    public class MessagePageRenderer : PageRenderer, Inbox.IFetchMessagesCallback
+#pragma warning restore CS0618 // Type or member is obsolete
+    {
+        private class WebViewClient : MessageWebViewClient
+        {
+            private MessagePageRenderer renderer;
+            private bool error = false;
+
+            public WebViewClient(MessagePageRenderer messagePageRenderer)
+            {
+                renderer = messagePageRenderer;
+            }
+
+            public override void OnPageFinished(WebView view, string url)
+            {
+                base.OnPageFinished(view, url);
+
+                if (renderer.message == null)
+                {
+                    return;
+                }
+
+                if (error)
+                {
+                    renderer.messagePage.OnRendererLoadFailed(renderer.message.MessageId, false, MessageFailureStatus.LoadFailed);
+                    return;
+                }
+
+                renderer.message.MarkRead();
+                renderer.messagePage.OnRendererLoaded(renderer.message.MessageId);
+            }
+
+#pragma warning disable CS0672 // Member overrides obsolete member
+            public override void OnReceivedError(WebView view, [GeneratedEnum] ClientError errorCode, string description, string failingUrl)
+#pragma warning restore CS0672 // Member overrides obsolete member
+            {
+#pragma warning disable CS0618 // Type or member is obsolete
+                base.OnReceivedError(view, errorCode, description, failingUrl);
+#pragma warning restore CS0618 // Type or member is obsolete
+
+                if (renderer.message != null && failingUrl != null && failingUrl.Equals(renderer.message.MessageBodyUrl))
+                {
+                    error = true;
+                }
+            }
+
+            protected override void OnClose(WebView webView)
+            {
+                base.OnClose(webView);
+                if (renderer.message != null)
+                {
+                    renderer.messagePage.OnRendererClosed(renderer.message.MessageId);
+                }
+            }
+        }
+
+        private UrbanAirship.MessageCenter.Message message;
+        private ICancelable fetchMessageRequest;
+        private MessageWebView webView;
+        private MessagePage messagePage;
+        private WebViewClient webViewClient;
+        private string messageId;
+        private FrameLayout nativeView;
+
+        public MessagePageRenderer(Context context) : base(context)
+        {
+            webView = new MessageWebView(Context);
+            webViewClient = new WebViewClient(this);
+
+            var fl = new FrameLayout(Context);
+            fl.LayoutParameters = new LayoutParams(LayoutParams.MatchParent, LayoutParams.MatchParent);
+
+            fl.AddView(webView);
+
+            webView.SetWebViewClient(webViewClient);
+            AddView(fl);
+
+            nativeView = fl;
+        }
+
+        public void LoadMessage(string messageId)
+        {
+            if (fetchMessageRequest != null)
+            {
+                fetchMessageRequest.Cancel();
+            }
+
+            message = null;
+            StartLoading(messageId);
+        }
+
+        private void StartLoading(string messageId)
+        {
+            this.message = MessageCenterClass.Shared().Inbox.GetMessage(messageId);
+            if (message == null)
+            {
+                fetchMessageRequest = MessageCenterClass.Shared().Inbox.FetchMessages(this);
+            }
+            else
+            {
+                if (message.IsExpired)
+                {
+                    messagePage.OnRendererLoadFailed(messageId, false, MessageFailureStatus.Unavailable);
+                    return;
+                }
+
+                webView.LoadMessage(message);
+                messagePage.OnRendererLoadStarted(messageId);
+            }
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Page> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.OldElement != null || Element == null)
+            {
+                return;
+            }
+
+            messagePage = e.NewElement as MessagePage;
+            messageId = messagePage.MessageId;
+
+            if (messageId != null)
+            {
+                LoadMessage(messageId);
+            }
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName.Equals(MessagePage.MessageIdProperty.PropertyName))
+            {
+                messageId = messagePage.MessageId;
+                if (messageId != null)
+                {
+                    LoadMessage(messageId);
+                }
+            }
+        }
+
+        protected override void OnLayout(bool changed, int l, int t, int r, int b)
+        {
+            base.OnLayout(changed, l, t, r, b);
+
+            if (nativeView != null)
+            {
+                var msw = MeasureSpec.MakeMeasureSpec(r - l, MeasureSpecMode.Exactly);
+                var msh = MeasureSpec.MakeMeasureSpec(b - t, MeasureSpecMode.Exactly);
+
+                nativeView.Measure(msw, msh);
+                nativeView.Layout(0, 0, r - l, b - t);
+            }
+        }
+
+        void Inbox.IFetchMessagesCallback.OnFinished(bool success)
+        {
+            message = MessageCenterClass.Shared().Inbox.GetMessage(messageId);
+            if (!success)
+            {
+                messagePage.OnRendererLoadFailed(messageId, true, MessageFailureStatus.FetchFailed);
+                return;
+            }
+            else if (message == null || message.IsExpired)
+            {
+                messagePage.OnRendererLoadFailed(messageId, false, MessageFailureStatus.Unavailable);
+                return;
+            }
+
+            webView.LoadMessage(message);
+            messagePage.OnRendererLoadStarted(messageId);
+        }
+    }
+}
---
+++ src/Airship.DotNet/Platforms/iOS/MessagePageRenderer.cs
@@ -0,0 +1,177 @@
+﻿/*
+ Copyright Airship and Contributors
+*/
+
+using CoreGraphics;
+using System;
+using System.ComponentModel;
+using Foundation;
+using UIKit;
+using UrbanAirship.DotNet.MessageCenter;
+using WebKit;
+using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Embedding;
+using Microsoft.Maui.Controls.Compatibility.Platform.iOS;
+using Microsoft.Maui.Controls.Platform;
+
+namespace UrbanAirship.DotNet.iOS
+{
+    // TODO(maui): update to handler/mapper
+#pragma warning disable CS0618 // Type or member is obsolete
+    public class MessagePageRenderer : PageRenderer, IUANavigationDelegate, IUANativeBridgeDelegate
+#pragma warning restore CS0618 // Type or member is obsolete
+    {
+        private WKWebView webView;
+        private UANativeBridge nativeBridge;
+        private MessagePage messagePage;
+        private string messageId;
+
+        public MessagePageRenderer()
+        {
+            CGRect frame = UIScreen.MainScreen.Bounds;
+            WKWebViewConfiguration configuration = new WKWebViewConfiguration();
+            webView = new WKWebView(frame, configuration);
+            webView.TranslatesAutoresizingMaskIntoConstraints = false;
+            nativeBridge = new UANativeBridge();
+            webView.NavigationDelegate = nativeBridge;
+            nativeBridge.ForwardNavigationDelegate = this;
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            webView.Frame = UIScreen.MainScreen.Bounds;
+            webView.AutoresizingMask = UIViewAutoresizing.FlexibleWidth | UIViewAutoresizing.FlexibleHeight;
+            webView.TranslatesAutoresizingMaskIntoConstraints = true;
+
+            View.AddSubview(webView);
+        }
+
+        public void LoadMessage(string messageId)
+        {
+            var message = UAMessageCenter.Shared.MessageList.Message(messageId);
+            if (message != null)
+            {
+                LoadMessageBody(message);
+            }
+            else
+            {
+                UAMessageCenter.Shared.MessageList.RetrieveMessageList(() =>
+                {
+                    message = UAMessageCenter.Shared.MessageList.Message(messageId);
+                    if (message != null && !message.IsExpired())
+                    {
+                        LoadMessageBody(message);
+                    }
+                    else
+                    {
+                        messagePage.OnRendererLoadFailed(messageId, false, MessageFailureStatus.Unavailable);
+                    }
+                }, () =>
+                {
+                    messagePage.OnRendererLoadFailed(messageId, false, MessageFailureStatus.FetchFailed);
+                });
+            }
+        }
+
+        public void Close()
+        {
+            messagePage.OnRendererClosed(messageId);
+        }
+
+        [Export("webView:decidePolicyForNavigationResponse:decisionHandler:")]
+        public void DecidePolicy(WKWebView webView, WKNavigationResponse navigationResponse, Action<WKNavigationResponsePolicy> decisionHandler)
+        {
+            var response = navigationResponse.Response as NSHttpUrlResponse;
+            if (response.StatusCode >= 400 && response.StatusCode <= 599)
+            {
+                decisionHandler(WKNavigationResponsePolicy.Cancel);
+                if (response.StatusCode == 410)
+                {
+                    messagePage.OnRendererLoadFailed(messageId, false, MessageFailureStatus.Unavailable);
+                }
+                else
+                {
+                    messagePage.OnRendererLoadFailed(messageId, true, MessageFailureStatus.LoadFailed);
+                }
+            }
+            else
+            {
+                decisionHandler(WKNavigationResponsePolicy.Allow);
+            }
+        }
+
+        [Export("webView:didFinishNavigation:")]
+        public void DidFinishNavigation(WKWebView webView, WKNavigation navigation)
+        {
+            messagePage.OnRendererLoaded(messageId);
+        }
+
+        [Export("webView:didFailNavigation:withError:")]
+        public void DidFailNavigation(WKWebView webView, WKNavigation navigation, NSError error)
+        {
+            messagePage.OnRendererLoadFailed(messageId, true, MessageFailureStatus.LoadFailed);
+        }
+
+        [Export("webView:didFailProvisionalNavigation:withError:")]
+        public void DidFailProvisionalNavigation(WKWebView webView, WKNavigation navigation, NSError error)
+        {
+            webView.NavigationDelegate?.DidFailNavigation(webView, navigation, error);
+        }
+
+        protected override void OnElementChanged(VisualElementChangedEventArgs e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.OldElement != null || Element == null)
+            {
+                return;
+            }
+
+            messagePage = e.NewElement as MessagePage;
+            messageId = messagePage.MessageId;
+            messagePage.PropertyChanged += OnElementPropertyChanged;
+
+            if (messageId != null)
+            {
+                LoadMessage(messageId);
+            }
+        }
+
+        private void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName.Equals(MessagePage.MessageIdProperty.PropertyName))
+            {
+                messageId = messagePage.MessageId;
+                if (messageId != null)
+                {
+                    LoadMessage(messageId);
+                }
+            }
+        }
+
+        private void LoadMessageBody(UAInboxMessage message)
+        {
+            var request = new NSMutableUrlRequest(message.MessageBodyURL);
+            UAMessageCenter.Shared.User.GetUserData((UAUserData userData) =>
+            {
+                if (userData == null)
+                {
+                    return;
+                }
+
+                var auth = UAUtils.AuthHeaderString(userData.Username, userData.Password);
+
+                NSMutableDictionary dict = new NSMutableDictionary();
+                dict.Add(new NSString("Authorization"), new NSString(auth));
+                request.Headers = dict;
+
+                webView.LoadRequest(request);
+
+                messagePage.OnRendererLoadStarted(messageId);
+            });
+        }
+    }
+}
