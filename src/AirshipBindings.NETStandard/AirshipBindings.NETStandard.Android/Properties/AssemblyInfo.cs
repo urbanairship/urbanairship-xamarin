c62a0bc40eb3acc8c3fd44fbf1f72c601cf31838
Josh Yaganeh <319444+jyaganeh@users.noreply.github.com>
2023-01-09 10:39:04 -0800

    Delete NETStandard binding

--- src/AirshipBindings.NETStandard/AirshipBindings.NETStandard.Android/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-using UrbanAirship.NETStandard.MessageCenter;
-using Xamarin.Forms;
-
-[assembly: AssemblyTitle("AirshipBindings.NETStandard.Android")]
-[assembly: ExportRenderer(typeof(MessagePage), typeof(UrbanAirship.NETStandard.Android.MessagePageRenderer))]
-
