e042ebd753844882b9487d25384769ac0df11303
Josh Yaganeh <319444+jyaganeh@users.noreply.github.com>
2023-01-09 16:31:36 -0800

    Airship.Net tweaks

---
+++ src/Airship.Net/CustomEvent.cs
@@ -0,0 +1,122 @@
+﻿/* Copyright Airship and Contributors */
+
+namespace Airship.Net.Analytics
+{
+    /// <summary>
+    /// A Custom Event model object.
+    /// </summary>
+    public class CustomEvent
+    {
+        // FIXME: should it be nullable or should we fix constructor?
+        private string? eventValue;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CustomEvent"/> class.
+        /// </summary>
+        public CustomEvent()
+        {
+            PropertyList = new List<IProperty>();
+        }
+
+        /// <summary>
+        /// Gets or sets the name of the event.
+        /// </summary>
+        /// <value>The name of the event.</value>
+        public string EventName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the event value.
+        /// </summary>
+        /// <value>The event value.</value>
+        public double EventValue
+        {
+            get => double.Parse(eventValue);
+            set => eventValue = value.ToString();
+        }
+
+        /// <summary>
+        /// Gets or sets the transaction identifier.
+        /// </summary>
+        /// <value>The transaction identifier.</value>
+        public string TransactionId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the interaction type.
+        /// </summary>
+        /// <value>The interaction type.</value>
+        public string InteractionType { get; set; }
+
+        /// <summary>
+        /// Gets or sets the interaction identifier.
+        /// </summary>
+        /// <value>The interaction identifier.</value>
+        public string InteractionId { get; set; }
+
+        /// <summary>
+        /// Gets a list of properties.
+        /// </summary>
+        /// <value>The property list.</value>
+        public List<IProperty> PropertyList { get; private set; }
+
+        /// <summary>
+        /// Adds a string property.
+        /// </summary>
+        /// <param name="name">The property name.</param>
+        /// <param name="value">The property value.</param>
+        public void AddProperty(string name, string value)
+        {
+            PropertyList.Add(new Property<string>(name, value));
+        }
+
+        /// <summary>
+        /// Adds a double property.
+        /// </summary>
+        /// <param name="name">The property name.</param>
+        /// <param name="value">The property value.</param>
+        public void AddProperty(string name, double value)
+        {
+            PropertyList.Add(new Property<double>(name, value));
+        }
+
+        /// <summary>
+        /// Adds a bool property.
+        /// </summary>
+        /// <param name="name">The property name.</param>
+        /// <param name="value">The property value.</param>
+        public void AddProperty(string name, bool value)
+        {
+            PropertyList.Add(new Property<bool>(name, value));
+        }
+
+        /// <summary>
+        /// Adds a string array property.
+        /// </summary>
+        /// <param name="name">The property name.</param>
+        /// <param name="value">The property value.</param>
+        public void AddProperty(string name, ICollection<string> value)
+        {
+            PropertyList.Add(new Property<string[]>(name, value.ToArray()));
+        }
+
+        //@cond IGNORE
+
+        public class Property<T> : IProperty
+        {
+            public string Name { get; private set; }
+            public T Value { get; private set; }
+
+            public Property(string name, T value)
+            {
+                Name = name;
+                Value = value;
+            }
+        }
+
+        public interface IProperty
+        {
+            string Name { get; }
+        }
+
+        //@endcond
+    }
+}
---
+++ src/Airship.Net/IAirship.cs
@@ -0,0 +1,112 @@
+﻿/* Copyright Airship and Contributors */
+
+using Airship.Net.Attributes;
+using Airship.Net.Channel;
+
+namespace Airship.Net
+{
+
+    public class ChannelEventArgs : EventArgs
+    {
+        public string ChannelId { get; private set; }
+
+        public ChannelEventArgs(string channelId)
+        {
+            ChannelId = channelId;
+        }
+    }
+
+    public class DeepLinkEventArgs : EventArgs
+    {
+        public string DeepLink { get; internal set; }
+        public DeepLinkEventArgs(string deepLink)
+        {
+            DeepLink = deepLink;
+        }
+    }
+
+    public class MessageCenterEventArgs : EventArgs
+    {
+        public string? MessageId { get; internal set; }
+        public MessageCenterEventArgs(string? messageId = null)
+        {
+            MessageId = messageId;
+        }
+    }
+
+    [Flags]
+    public enum Features
+    {
+        None = 0,
+        InAppAutomation = 1 << 0,
+        MessageCenter = 1 << 1,
+        Push = 1 << 2,
+        Chat = 1 << 3,
+        Analytics = 1 << 4,
+        TagsAndAttributes = 1 << 5,
+        Contacts = 1 << 6,
+        Location = 1 << 7,
+        All = InAppAutomation | MessageCenter | Push | Chat | Analytics | TagsAndAttributes | Contacts | Location
+    }
+
+    public interface IAirship
+    {
+        bool UserNotificationsEnabled { get; set; }
+
+        Features EnabledFeatures { get; set; }
+
+        void EnableFeatures(Features features);
+
+        void DisableFeatures(Features features);
+
+        bool IsFeatureEnabled(Features feature);
+
+        bool IsAnyFeatureEnabled();
+
+        IEnumerable<string> Tags { get; }
+
+        string? ChannelId { get; }
+
+        string? NamedUser { get; set; }
+
+        event EventHandler<ChannelEventArgs> OnChannelCreation;
+
+        event EventHandler<ChannelEventArgs> OnChannelUpdate;
+
+        event EventHandler<DeepLinkEventArgs> OnDeepLinkReceived;
+
+        event EventHandler<MessageCenterEventArgs> OnMessageCenterDisplay;
+
+        TagEditor EditDeviceTags();
+
+        void AddCustomEvent(Analytics.CustomEvent customEvent);
+
+        void TrackScreen(string screen);
+
+        void AssociateIdentifier(string key, string identifier);
+
+        void DisplayMessageCenter();
+
+        int MessageCenterUnreadCount { get; }
+
+        int MessageCenterCount { get; }
+
+        List<MessageCenter.Message> InboxMessages { get; }
+
+        TagGroupsEditor EditNamedUserTagGroups();
+
+        TagGroupsEditor EditChannelTagGroups();
+
+        AttributeEditor EditAttributes();
+
+        event EventHandler OnMessageCenterUpdated;
+
+        AttributeEditor EditChannelAttributes();
+
+        AttributeEditor EditNamedUserAttributes();
+
+        bool InAppAutomationPaused { get; set; }
+
+        TimeSpan InAppAutomationDisplayInterval { get; set; }
+    }
+}
