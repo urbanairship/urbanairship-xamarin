0c1253c4b27a6b64196444158ad12d374f737f45
Josh Yaganeh <319444+jyaganeh@users.noreply.github.com>
2023-02-14 17:48:41 -0800

    Remove old Xamarin sample

--- SampleApp/SampleApp/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-/*
- Copyright Airship and Contributors
-*/
-
-using Xamarin.Forms.Xaml;
-using System.Resources;
-
-[assembly: NeutralResourcesLanguage("en-US")]
-[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
