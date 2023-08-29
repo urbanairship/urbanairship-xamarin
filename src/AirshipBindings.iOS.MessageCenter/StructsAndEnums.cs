8b66a8fe9e41d781b5245a5484c1a76e5e44902c
khmMouna <mouna.khemiri@airship.com>
2023-08-29 15:25:58 +0200

    Update message center bindings

--- src/AirshipBindings.iOS.MessageCenter/StructsAndEnums.cs
+++ src/AirshipBindings.iOS.MessageCenter/StructsAndEnums.cs
@@ -7,18 +7,4 @@ using UIKit;
 
 namespace UrbanAirship {
 
-    [Native]
-    public enum UAMessageCenterMessageLoadErrorCode : long
-    {
-        ListUnavailable = 0,
-        FailureStatus = 1,
-        MessageExpired = 2
-    }
-
-    [Native]
-    public enum UANavigationBarStyle : ulong
-    {
-        Default = 0,
-        Black = 1
-    }
 }
