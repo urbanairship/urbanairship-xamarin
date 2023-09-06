e101da6dbff653f311ff7f6ee5293e2eb0b11e88
khmMouna <mouna.khemiri@airship.com>
2023-09-06 10:05:44 +0200

    Revert "Update message center bindings"
    This reverts commit 8b66a8fe9e41d781b5245a5484c1a76e5e44902c.

--- src/AirshipBindings.iOS.MessageCenter/StructsAndEnums.cs
+++ src/AirshipBindings.iOS.MessageCenter/StructsAndEnums.cs
@@ -7,4 +7,18 @@ using UIKit;
 
 namespace UrbanAirship {
 
+    [Native]
+    public enum UAMessageCenterMessageLoadErrorCode : long
+    {
+        ListUnavailable = 0,
+        FailureStatus = 1,
+        MessageExpired = 2
+    }
+
+    [Native]
+    public enum UANavigationBarStyle : ulong
+    {
+        Default = 0,
+        Black = 1
+    }
 }
