3649b57c67cfd694ff2ab27c5d5167450f627638
Josh Yaganeh <319444+jyaganeh@users.noreply.github.com>
2024-03-15 15:03:24 -0700

    Bump SDKs to Android 17.7.3, iOS 17.9.0

--- src/SharedAssemblyInfo.CrossPlatform.cs
+++ src/SharedAssemblyInfo.CrossPlatform.cs
@@ -12,5 +12,5 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion ("18.1.0")]
+[assembly: AssemblyVersion ("18.2.0")]
 
