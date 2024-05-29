86d0ce8c7f2e381df7004f1f6dd1d0c74018e14f
Josh Yaganeh <319444+jyaganeh@users.noreply.github.com>
2024-05-29 16:12:55 -0700

    Release 18.3.0 (#230)

--- src/SharedAssemblyInfo.Common.cs
+++ src/SharedAssemblyInfo.Common.cs
@@ -6,4 +6,4 @@ using UrbanAirship.Attributes;
 // Change them to the values specific to your project.
 
 // Cross-platform version of the plugin
-[assembly: UACrossPlatformVersion ("18.2.0")]
+[assembly: UACrossPlatformVersion ("18.3.0")]
